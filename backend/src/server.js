6fa823eb144c41f53ad8b855b8572c519c4309c0
Raih1107 <raih68737@gmail.com>
2025-06-29 02:09:24 +0530

    feat: integrate React Router and React Query for improved navigation and data fetching
    - Added routing with React Router for multiple pages: Home, Sign Up, Login, Notifications, Call, Chat, and Onboarding.
    - Implemented React Query for data fetching, including user authentication.
    - Created new pages: HomePage, SignUpPage, LoginPage, NotificationsPage, CallPage, ChatPage, and OnboardingPage.
    - Added axios instance for API calls and a signup function.
    - Updated package.json to include new dependencies: @tanstack/react-query, axios, lucide-react, react-hot-toast, and daisyui.
    - Configured Tailwind CSS with DaisyUI for styling.
    - Removed unused assets and CSS files.
    - Added PostCSS configuration for Tailwind CSS.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import "dotenv/config" ;
+import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
@@ -10,6 +11,10 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT ;
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
 app.use(cookieParser());
 app.use(express.json());
 
